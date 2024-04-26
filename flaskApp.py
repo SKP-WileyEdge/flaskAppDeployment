9eb040ab73e01691464d7ba86922b437ea266fa2
SKP-WileyEdge <kpsheetal7@gmail.com>
2024-04-26 14:24:36 +0530

    third commit

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,12 @@
+# This is a sample flask application that prints Hello world!
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from Sheetal!"
+@app.route("/")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello():
+    return "<h2>Hello from Sheetal !</h2><hr/>"
+
+app.run(host="0.0.0.0", port=5000)
